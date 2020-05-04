ae89d77c686db4a72832baff9517f44bc9493f5b
juyeonblue <jyp0211@ajou.ac.kr>
2020-05-04 23:44:59 +0900

    Add html files

--- webproject_5_MTV/myproject/myapp/views.py
+++ webproject_5_MTV/myproject/myapp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from myapp.models import Lion
 
 def index(request):
     return render(request, 'index.html') 
+
+def lion_list(request):
+    lion_list = Lion.objects.all()
+
+    return render(request, 'lions.html', {'lion_list' : lion_list})
