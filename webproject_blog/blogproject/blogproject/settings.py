68d8195638614621637d829d31168819ca49581f
juyeonblue <jyp0211@ajou.ac.kr>
2020-05-12 02:21:24 +0900

    Add media file

--- webproject_blog/blogproject/blogproject/settings.py
+++ webproject_blog/blogproject/blogproject/settings.py
@@ -126,3 +126,7 @@ STATICFILES_DIRS = [
 ] # static 파일들이 현재 어디에 있는지를 쓰는 곳 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') #static 파일들이 어디로 모일 것인지 쓰는 곳
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- webproject_blog/blogproject/blogproject/urls.py
+++ webproject_blog/blogproject/blogproject/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 import blogapp.views
 import portfolio.views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.home, name = "home"),
@@ -10,4 +13,4 @@ urlpatterns = [
     path('blog/new/', blogapp.views.new, name = "new"),
     path('blog/create', blogapp.views.create, name = "create"), #html을 띄운다는 것이 아니라 create 함수를 실행시킨다는 뜻
     path('portfolio/', portfolio.views.portfolio, name = "portfolio"),
-    ]
+    ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
