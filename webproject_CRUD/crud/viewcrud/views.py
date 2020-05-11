2a5df3c44d90d842ee1315848c4eac0415d79f75
juyeonblue <jyp0211@ajou.ac.kr>
2020-05-12 04:40:57 +0900

    complete CRUD project

--- webproject_CRUD/crud/viewcrud/views.py
+++ webproject_CRUD/crud/viewcrud/views.py
@@ -10,12 +10,6 @@ def read(request):
     blogs = Blog.objects.all()
     return render(request, 'viewcrud/funccrud.html', {'blogs': blogs})
 
-def update(request):
-    return
-
-def delete(request):
-    return
-
 def create(request): 
     # 새로운 데이터 블로그 글 저장하는 역할 == POST
     if request.method == 'POST':
@@ -29,3 +23,21 @@ def create(request):
     else: #글쓰기 페이지를 띄어주는 역할 == GET
         form = NewBlog()
         return render(request, 'viewcrud/new.html', {'form': form})
+
+def update(request, pk):
+    # 어떤 블로그를 수정할지 블로그 객체 갖고오기
+     blog = get_object_or_404(Blog, pk = pk)
+
+    # 해당하는 블로그 객체 pk에 맞는 입력공간
+     form = NewBlog(request.POST, instance=blog)
+
+     if form.is_valid():
+        form.save()
+        return redirect('home')
+
+     return render(request, 'viewcrud/new.html', {'form': form})
+
+def delete(request, pk):
+    blog = get_object_or_404(Blog, pk = pk)
+    blog.delete()
+    return redirect('home')
