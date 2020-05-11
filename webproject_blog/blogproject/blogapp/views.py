ac8c0f9fe4bd2adf1303a333279ae148cb0c3c83
juyeonblue <jyp0211@ajou.ac.kr>
2020-05-12 00:02:47 +0900

    Create navbar

--- webproject_blog/blogproject/blogapp/views.py
+++ webproject_blog/blogproject/blogapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Blog
 
 def home(request):
@@ -9,3 +10,15 @@ def detail(request, blog_id):
     details = get_object_or_404(Blog, pk = blog_id)
     return render(request, 'blogapp/detail.html', {'details': details})
 
+def new(request):
+    return render(request, 'blogapp/new.html')
+
+def create(request):
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save() #blog객체에 해당하는 내용을 저장해라
+    return redirect('/blog/' + str(blog.id)) #위의 내용을 이 url로 넘기세요 
+    #url은 항상 정수형인데 id는 인트형이므로 str을 사용하여 형 변환한다
+    
--- webproject_blog/blogproject/blogproject/urls.py
+++ webproject_blog/blogproject/blogproject/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.home, name = "home"),
     path('blog/<int:blog_id>', blogapp.views.detail, name = "detail"),
-]
+    path('blog/new/', blogapp.views.new, name = "new"),
+    path('blog/create', blogapp.views.create, name = "create"), #html을 띄운다는 것이 아니라 create 함수를 실행시킨다는 뜻
+    ]
