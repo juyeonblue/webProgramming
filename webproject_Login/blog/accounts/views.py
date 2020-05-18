d2630a050421bb15a0069d90a7f51145ff73e875
juyeonblue <jyp0211@ajou.ac.kr>
2020-05-18 23:39:33 +0900

    setting files

--- webproject_Login/blog/accounts/views.py
+++ webproject_Login/blog/accounts/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+
+def home(request):
+    return render(request, 'accounts/login.html', name = 'login')
+
+def signup(request):
+    return render(request, 'accounts/signup.html', name= 'signtup')
